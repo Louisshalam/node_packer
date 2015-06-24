6a6b63fc3d3c4b141d148e1593a685bb29ace8ff
louisshalam <louisshalam@gmail.com>
2015-06-24 11:25:29 -0400

    Random files

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 /**
  * Created by louisshalam on 6/15/15.
  */
+
+"use strict";
+
+var fs = require("fs");
+var util = require("util");
+
+var emails = fs.readFileSync('/Users/louisshalam/GitHub/Packer/node_packer/resources/emails.txt', {encoding:'utf8'});
+console.log(util.inspect(emails));
+
+var emailsArray = emails.split('\n');
+
+console.log(emailsArray);
+
+var domains = {};
+
+emailsArray.forEach(function(email, index, array) {
+    var splitEmail = email.split('@');
+    var domain = splitEmail[1];
+    console.log(domain);
+    if(domains[domain]) {
+        domains[domain]++;
+    } else {
+        domains[domain] = 1;
+    }
+});
+
+console.log(domains);
